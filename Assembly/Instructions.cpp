de6d85853d2667cee84203c411aee63ef13a49a6
youssef3453 <63240173+youssef3453@users.noreply.github.com>
2023-05-10 23:59:58 +0300

    add LB instruction

--- Assembly/Instructions.cpp
+++ Assembly/Instructions.cpp
@@ -250,7 +250,17 @@ void LH(int rd, int rs1, int imm)
 }
 void LB(int rd, int rs1, int imm)
 {
-
+    if (rd == 0) return;
+    if (imm > ((1 << 11) - 1) || imm < -(1 << 11)) {
+        cout << "\"offset\" not in allowed range\n";
+        exit(1);
+    }
+    int content = memory[registers[rs1] + imm];
+    string binaryContent = decimalToBinary_Signed(content);
+    string sub = binaryContent.substr(24);
+    binaryContent = sub[0] == '1' ? "111111111111111111111111" + binaryContent : "000000000000000000000000" + binaryContent;
+    registers[rd] = binaryToDecimal(binaryContent);
+    PC += 4;
 }
 void LHU(int rd, int rs1, int imm)
 {
