099871f8c8181b145435ba2dd918f2a53e917a22
Omar Bahgat <omar_bahgat@aucegypt.edu>
2023-05-10 12:05:27 +0300

    Update Instructions.h

--- Instructions.h
+++ Instructions.h
@@ -8,6 +8,13 @@ using namespace std;
 inline int PC = 0;                 // program counter
 inline int registers[32] = {0};    // initial value of all registers is 0
 inline map<int,int> memory;        // {address, value}
+inline map<int,vector<string>> addressToInstruction; // maps each address to instruction so I can access instruction using PC
+inline map<string,int> addressOfLabel;
+
+/*
+ IMPORTANT:
+ Take care of signed/unsigned
+ */
 
 // R-type   
 void ADD (int rd, int rs1, int rs2);    // done
@@ -17,18 +24,18 @@ void OR (int rd, int rs1, int rs2);     // done
 void XOR (int rd, int rs1, int rs2);    // done
 void SLL (int rd, int rs1, int rs2);    // done
 void SRL (int rd, int rs1, int rs2);    // done
-void SRA (int rd, int rs1, int rs2);
-void SLT (int rd, int rs1, int rs2);
-void SLTU (int rd, int rs1, int rs2);
+void SRA (int rd, int rs1, int rs2);    // done
+void SLT (int rd, int rs1, int rs2);    // done
+void SLTU (int rd, int rs1, int rs2);   // done
 
 // I-type
 void ADDI (int rd, int rs1, int imm);   // done
 void ANDI (int rd, int rs1, int imm);   // done
 void ORI (int rd, int rs1, int imm);    // done
 void XORI (int rd, int rs1, int imm);   // done
-void SLLI (int rd, int rs1, int imm);
-void SRLI (int rd, int rs1, int imm);
-void SRAI (int rd, int rs1, int imm);
+void SLLI (int rd, int rs1, int imm);   // done
+void SRLI (int rd, int rs1, int imm);   // done
+void SRAI (int rd, int rs1, int imm);   // done
 void JALR (int rd, int rs1, int imm);
 void LW (int rd, int base, int offset); // done
 void LH (int rd, int base, int offset);
@@ -42,3 +49,19 @@ void SLTIU (int rd, int rs1, int imm);
 void SW (int rs1, int base, int offset); // done
 void SH (int rs1, int base, int offset);
 void SB (int rs1, int base, int offset);
+
+// B-type
+void BEQ (int rs1, int rs2, string label);
+void BEQ (int rs1, int rs2, int offset);
+void BNE (int rs1, int rs2, string label);
+void BNE (int rs1, int rs2, int offset);
+void BLT (int rs1, int rs2, string label);
+void BLT (int rs1, int rs2, int offset);
+void BLTU (int rs1, int rs2, string label);
+void BLTU (int rs1, int rs2, int offset);
+void BGE (int rs1, int rs2, string label);
+void BGE (int rs1, int rs2,int offset);
+void BGEU (int rs1, int rs2, string label);
+void BGEU (int rs1, int rs2,int offset);
+
+
